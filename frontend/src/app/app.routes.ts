823ccebb14e5e644b2a5a2405468bb2bd353ff7d
kirrer-izo <isaackmuchoki55@gmail.com>
2026-01-28 16:06:46 +0300

    feat: implement frontend auth service and login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LiveYourBooksComponent } from './pages/live-your-books/live-your-books.component';
 import { SoleProprietorCRMComponent } from './pages/sole-proprietor-crm/sole-proprietor-crm.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'projects/live-your-books', component: LiveYourBooksComponent },
-    { path: 'projects/sole-proprietor-crm', component: SoleProprietorCRMComponent }
+    { path: 'projects/sole-proprietor-crm', component: SoleProprietorCRMComponent },
+    { path: 'login', component: LoginComponent }
 ];
