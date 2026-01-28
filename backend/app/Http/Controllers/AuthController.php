57e3bbb37607787bc252d452dd5d55eb342eedcd
kirrer-izo <isaackmuchoki55@gmail.com>
2026-01-28 16:06:11 +0300

    fix: add AuthController use in app.php and comment middleware check construct

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function __construct() {
-        $this->middleware('auth:sanctum')->except('login');
-    }
+    // public function __construct() {
+    //     $this->middleware('auth:sanctum')->except('login');
+    // }
     public function login(Request $request)
     {
         $credentials = $request->validate([
